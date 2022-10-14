bedf85a1ca675e11befc7d4aa9a74b049036dc58
Nathan Galmiche <ngalmiche-22@tp-1a252-28.enst.fr>
2022-10-14 12:41:58 +0200

    deb_TD_geometry

--- TP_geometry_shaders/pogl_skel_tp/main.cpp
+++ TP_geometry_shaders/pogl_skel_tp/main.cpp
@@ -110,21 +110,26 @@ std::string load(const std::string &filename) {
 
 bool init_shaders() {
   std::string vertex_src = load("vertex.shd");
+  std::string geometry_src = load("geometry.shd");
   std::string fragment_src = load("fragment.shd");
-  GLuint shader_id[2];
+  GLuint shader_id[3];
   GLint compile_status = GL_TRUE;
   char *vertex_shd_src = (char*)std::malloc(vertex_src.length()*sizeof(char));
+  char *geometry_shd_src = (char*)std::malloc(geometry_src.length()*sizeof(char));
   char *fragment_shd_src = (char*)std::malloc(fragment_src.length()*sizeof(char));
   vertex_src.copy(vertex_shd_src,vertex_src.length());
+  geometry_src.copy(geometry_shd_src,geometry_src.length());
   fragment_src.copy(fragment_shd_src,fragment_src.length());
 
 
   shader_id[0] = glCreateShader(GL_VERTEX_SHADER);TEST_OPENGL_ERROR();
-  shader_id[1] = glCreateShader(GL_FRAGMENT_SHADER);TEST_OPENGL_ERROR();
+  shader_id[1] = glCreateShader(GL_GEOMETRY_SHADER);TEST_OPENGL_ERROR();
+  shader_id[2] = glCreateShader(GL_FRAGMENT_SHADER);TEST_OPENGL_ERROR();
 
   glShaderSource(shader_id[0], 1, (const GLchar**)&(vertex_shd_src), 0);TEST_OPENGL_ERROR();
-  glShaderSource(shader_id[1], 1, (const GLchar**)&(fragment_shd_src), 0);TEST_OPENGL_ERROR();
-  for(int i = 0 ; i < 2 ; i++) {
+  glShaderSource(shader_id[1], 1, (const GLchar**)&(geometry_shd_src), 0);TEST_OPENGL_ERROR();
+  glShaderSource(shader_id[2], 1, (const GLchar**)&(fragment_shd_src), 0);TEST_OPENGL_ERROR();
+  for(int i = 0 ; i < 3 ; i++) {
     glCompileShader(shader_id[i]);TEST_OPENGL_ERROR();
     glGetShaderiv(shader_id[i], GL_COMPILE_STATUS, &compile_status);
     if(compile_status != GL_TRUE) {
@@ -133,25 +138,28 @@ bool init_shaders() {
       glGetShaderiv(shader_id[i], GL_INFO_LOG_LENGTH, &log_size);
       shader_log = (char*)std::malloc(log_size+1); /* +1 pour le caractere de fin de chaine '\0' */
       if(shader_log != 0) {
-	glGetShaderInfoLog(shader_id[i], log_size, &log_size, shader_log);
-	std::cerr << "SHADER " << i << ": " << shader_log << std::endl;
-	std::free(shader_log);
+        glGetShaderInfoLog(shader_id[i], log_size, &log_size, shader_log);
+        std::cerr << "SHADER " << i << ": " << shader_log << std::endl;
+        std::free(shader_log);
       }
       std::free(vertex_shd_src);
+      std::free(geometry_shd_src);
       std::free(fragment_shd_src);
       glDeleteShader(shader_id[0]);
       glDeleteShader(shader_id[1]);
+      glDeleteShader(shader_id[2]);
       return false;
     }
   }
   std::free(vertex_shd_src);
+  std::free(geometry_shd_src);
   std::free(fragment_shd_src);
 
 
   GLint link_status=GL_TRUE;
   program_id=glCreateProgram();TEST_OPENGL_ERROR();
   if (program_id==0) return false;
-  for(int i = 0 ; i < 2 ; i++) {
+  for(int i = 0 ; i < 3 ; i++) {
     glAttachShader(program_id, shader_id[i]);TEST_OPENGL_ERROR();
   }
   glLinkProgram(program_id);TEST_OPENGL_ERROR();
@@ -169,6 +177,7 @@ bool init_shaders() {
     glDeleteProgram(program_id);TEST_OPENGL_ERROR();
     glDeleteShader(shader_id[0]);TEST_OPENGL_ERROR();
     glDeleteShader(shader_id[1]);TEST_OPENGL_ERROR();
+    glDeleteShader(shader_id[2]);TEST_OPENGL_ERROR();
     program_id=0;
     return false;
   }
