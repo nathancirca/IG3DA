af0b987b3f3332c2928086a7f1557610f28762d5
Nathan Galmiche <ngalmiche-22@tp-1a252-16.enst.fr>
2022-10-21 11:59:07 +0200

    tp_reconstruction

---
+++ ReconstructionTP/CMakeFiles/Reconstruct.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Reconstruct.dir/reconstruct.cpp.o"
+  "CMakeFiles/Reconstruct.dir/reconstruct.cpp.o.d"
+  "Reconstruct"
+  "Reconstruct.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Reconstruct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
